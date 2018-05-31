398b5a809dba3eb4039aef00366de80612788e1b
Allis Tauri <allista@gmail.com>
2018-05-31 09:57:53 +0300

    Added animated anchor module. Just a normal ATGroundAnchor with animation.

---
+++ AnimatedGroundAnchor.cs
@@ -0,0 +1,35 @@
+﻿//   AnimatedGroundAnchor.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2018 Allis Tauri
+namespace AT_Utils
+{
+    public class AnimatedGroundAnchor : ATGroundAnchor
+    {
+        [KSPField] public string AnimatorID = string.Empty;
+        MultiAnimator Animator;
+
+		public override void OnStart(StartState state)
+		{
+            base.OnStart(state);
+            if(!string.IsNullOrEmpty(AnimatorID))
+                Animator = part.GetAnimator(AnimatorID);
+		}
+
+		protected override void on_anchor_attached()
+		{
+            base.on_anchor_attached();
+            if(Animator != null)
+                Animator.Open();
+		}
+
+		protected override void on_anchor_detached()
+		{
+            base.on_anchor_detached();
+            if(Animator != null)
+                Animator.Close();
+		}
+	}
+}
